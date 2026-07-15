9ded74adbeb95a9ad1841c16016a68638c75fa70
openpobo <openpobo@gmail.com>
2026-07-15 19:53:32 +0700

    Create jajal.js

---
+++ functions/p/jajal.js
@@ -0,0 +1,18 @@
+import { layout } from "../../lib/render";
+import { canonical } from "../../lib/config";
+
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+
+  return layout({
+    page: "page",
+    title: "Update Terbaru p jajal",
+    description: "Update terbaru p  jajal",
+    canonical: url.origin + url.pathname,
+    content: `
+      <h1>Update Terbaru  jajal</h1>
+
+      <p>Isi artikel...</p>
+    `
+  });
+}
