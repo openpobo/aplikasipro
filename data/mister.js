c387e8ef2f714fb76409286a60c7d51344a2fb0c
openpobo <openpobo@gmail.com>
2026-07-15 19:47:32 +0700

    Update mister.js

--- data/mister.js
+++ data/mister.js
@@ -1,11 +1,11 @@
 export default {
   slug: "update-terbaru",
   kategori: "blog",
-  title: "Update Terbaru",
-  description: "Update terbaru Ai Mr Ferdy",
+  title: "Update Terbaru Mister",
+  description: "Update terbaru Mister",
   content: `
     <h2>Versi Terbaru</h2>
-    <p>Isi artikel sangat panjang...</p>
+    <p>Isi artikel sangat panjang Mister...</p>
 
     <h2>Perubahan</h2>
     <p>...</p>
