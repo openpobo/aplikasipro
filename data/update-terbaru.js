6f20ef047117b94fb02736f2979f770c8c65b21e
openpobo <openpobo@gmail.com>
2026-07-15 19:33:15 +0700

    Create update-terbaru.js

---
+++ data/update-terbaru.js
@@ -0,0 +1,13 @@
+export default {
+  title: "Update Terbaru",
+  description: "Update terbaru Ai Mr Ferdy.",
+  content: `
+<h1>Update Terbaru</h1>
+
+<p>Isi artikel panjang...</p>
+
+<h2>Versi 2.0</h2>
+
+<p>....</p>
+`
+};
