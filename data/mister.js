4f4ed3168e05381a2b746901b909129656bac4e7
openpobo <openpobo@gmail.com>
2026-07-15 19:42:53 +0700

    Create mister.js

---
+++ data/mister.js
@@ -0,0 +1,16 @@
+export default {
+  slug: "update-terbaru",
+  kategori: "blog",
+  title: "Update Terbaru",
+  description: "Update terbaru Ai Mr Ferdy",
+  content: `
+    <h2>Versi Terbaru</h2>
+    <p>Isi artikel sangat panjang...</p>
+
+    <h2>Perubahan</h2>
+    <p>...</p>
+
+    <h2>Download</h2>
+    <p>...</p>
+  `
+};
